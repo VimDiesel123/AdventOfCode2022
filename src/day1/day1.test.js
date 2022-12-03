b1aecc16e778956b1877e967c0f7f7bb335c402e
EddieSpaghetti666 <Dsmith141996@gmail.com>
2022-12-03 15:07:52 -0500

    Set up eslint

--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -1,10 +1,9 @@
-const { mostCalories, topThreeCalories}= require("./day1");
-
+const { mostCalories, topThreeCalories } = require('./day1');
 
 test('Most calories for test input should be 24000', () => {
-  expect(mostCalories("./test_input.txt")).toBe(24000);
-})
+  expect(mostCalories('./test_input.txt')).toBe(24000);
+});
 
-test('Sum of top three elves calories should be 45000', () =>{
+test('Sum of top three elves calories should be 45000', () => {
   expect(topThreeCalories('./test_input.txt')).toBe(45000);
-})
+});
